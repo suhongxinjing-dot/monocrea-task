1910536d56e8f3caaa9244862322ed29e3c89372
新井粛弘 <suhongxinjing@gmail.com>
2026-04-16 08:14:41 +0900

    再作成_フロント作成済み

--- forntend/mono-front/src/routes/+page.server.js
+++
@@ -1,7 +0,0 @@
-// src/routes/+page.server.js
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-    // ルート (/) に来たら、即座に /users へリダイレクト
-    throw redirect(302, '/users');
-}
--- forntend/mono-front/src/routes/users/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { getUsers } from '$lib/api/users';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    const users = await getUsers();
-    return {
-        users: users || []
-    };
-}
